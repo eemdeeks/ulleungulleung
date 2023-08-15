f1a88e731b833473829c48c632603dbbae34b486
JJunghyunY <wjdgus995@hanmail.net>
2023-08-15 13:15:41 +0900

    add line object

--- Ulleungton/ViewController.swift
+++ Ulleungton/ViewController.swift
@@ -9,37 +9,47 @@ import CoreMotion
 import UIKit
 
 final class ViewController: UIViewController {
-    @IBOutlet weak var objectImageView: UIImageView!
-    private let motionManager = CMMotionManager()
+	@IBOutlet weak var objectImageView: UIImageView!
+	@IBOutlet weak var lineView: UIImageView!
+	private let motionManager = CMMotionManager()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupMotionManager()
-        setupObjectImageView()
-    }
-    
-    private func setupMotionManager() {
-        guard motionManager.isAccelerometerAvailable else {
-            return
-        }
-        motionManager.accelerometerUpdateInterval = 0.01
-        motionManager.startAccelerometerUpdates(to: .main) { [unowned self] (data, error) in
-            guard let acceleration = data?.acceleration else {
-                return
-            }
-            updateRotation(
-                of: objectImageView,
-                angle: acceleration.x * -1
-            )
-        }
-    }
-    
-    private func setupObjectImageView() {
-        objectImageView.image = UIImage(named: "object")
-    }
-    
-    private func updateRotation(of view: UIView, angle: CGFloat) {
-        view.layer.anchorPoint = .init(x: 0.5, y: 1)
-        view.transform = .init(rotationAngle: angle)
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setupMotionManager()
+		setupImageView()
+	}
+	
+	private func setupMotionManager() {
+		guard motionManager.isAccelerometerAvailable else {
+			return
+		}
+		motionManager.accelerometerUpdateInterval = 0.01
+		motionManager.startAccelerometerUpdates(to: .main) { [unowned self] (data, error) in
+			guard let acceleration = data?.acceleration else {
+				return
+			}
+			updateRotation(
+				of: objectImageView,
+				angle: acceleration.x * -1
+			)
+			updateRotation(
+				of: lineView,
+				angle: (acceleration.x * -1) / 2
+			)
+		}
+	}
+	
+	private func setupImageView() {
+		objectImageView.image = UIImage(named: "object")
+		lineView.image = UIImage(named: "line")
+		
+		lineView.frame.origin.y = objectImageView.frame.maxY - 60
+		lineView.center.x = objectImageView.center.x
+	}
+	
+	
+	private func updateRotation(of view: UIView, angle: CGFloat) {
+		view.layer.anchorPoint = .init(x: 0.5, y: 1)
+		view.transform = .init(rotationAngle: angle)
+	}
 }
